e2ab0f407fbc6517d9fb110c681d5f4e245e2d06
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-02-18 21:40:18 +0530

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,35 @@ import Call from "./pages/Call/Call";
 import Navbar from "../src/components/Navbar/Navbar";
 import Footer from "../src/components/Footer/Footer";
 import Work from "./pages/Work/Work";
+import Loader from "./components/Loader";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <div className="font-poppins">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/call" element={<Call />} />
-          <Route path="/work" element={<Work />} />
-        </Routes>
-        <Footer />
-      </div>
+      {isLoading ? (
+        <div>
+          <Loader />
+        </div>
+      ) : (
+        <div className="font-poppins">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/call" element={<Call />} />
+            <Route path="/work" element={<Work />} />
+          </Routes>
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ module.exports = {
         oswald: ["Oswald", "sans-serif"],
         bebasNeue: ["Bebas Neue", "sans-serif"],
       },
+      animation: {
+        "spin-glow":
+          "spin 1s linear infinite, glow 1.5s ease-in-out infinite alternate",
+      },
+      keyframes: {
+        glow: {
+          "0%": { boxShadow: "0 0 10px #FF6F61" },
+          "100%": { boxShadow: "0 0 30px #FF6F61" },
+        },
+      },
     },
   },
   plugins: [],
